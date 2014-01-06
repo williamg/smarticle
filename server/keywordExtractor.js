1016af7c67cc11c3f974ec8f2d99c74580bbe2a1
William Ganucheau <williamg.online@gmail.com>
2014-01-05 19:03:01 -0600

    Added code to parse and save articles. Still very messy, needs lots of work.

---
+++ server/keywordExtractor.js
@@ -0,0 +1,153 @@
+// Stripped down/cleaned up version of "Gramophone" by "bxjx" on GitHub
+// https://github.com/bxjx/gramophone
+var _ = require ('lodash');
+var natural = require ('natural');
+var util = require ('util');
+var stopWords = require (__dirname + '/../stopwords.json');
+
+
+// =============================================================================
+// Keyword Detection
+// =============================================================================
+exports.extract = function (text) {
+	var results = [];
+	var keywords = {};
+	var combined, combinedResults = {};
+	
+	// Defaults
+	var options = {};
+	options.ngrams = [1, 2, 3];
+	options.cutoff = 0.5;
+	options.min = 2;
+
+	// Loop through each ngram (1, 2, 3)	
+	_.each (options.ngrams, function(ngram) {
+		// Tokenize the text
+		var tokenRegex = /[^a-zA-z'’0-9]/;
+		var tokenizer = new natural.RegexpTokenizer ({
+			pattern: tokenRegex
+		});
+		var tokenized = tokenizer.tokenize (text);	
+
+		// Combine each array of tokens to a single string
+		tokenized = _.map (natural.NGrams.ngrams (tokenized, ngram),
+			function (ngram) {
+				return ngram.join (' ').toLowerCase ();
+		});	
+
+		// Sort the keywords by frequency and eliminate blacklisted
+		// words		
+		var keywordsForNgram;
+		var tf = new Tf ();
+
+		tf.addDocument (tokenized);
+		keywordsForNgram = tf.listMostFrequentTerms (0);
+		keywordsForNgram = _.select (keywordsForNgram, function (item) {
+			return usePhrase (item.term, options);
+		});
+			
+		// Add to any existing keywords
+		results = results.concat (keywordsForNgram);
+	});
+
+	// Combine all the results
+	_.each (results, function (result) {
+		combinedResults[result.term] = result.tf;
+	});
+
+	// Filter redundant/duplicate keywords
+	combined = exports.combine (combinedResults, options.cutoff);
+
+	// Fancy mumbo-jumbo
+	combined = _.chain (combined)
+		.pairs ()
+		.sortBy (_.last)
+		.reverse ()
+		.map (function (combination) { 
+			return {
+				term: combination[0], 
+				tf: combination[1] 
+			};
+		})
+		.value();
+
+		combined = _.select (combined, function (result) {
+			return result.tf > options.min;
+		});
+
+		// Return the keywords
+		return combined;
+};
+
+// Copy-pasta from original Gramophone docs:
+// Attempt to combine the results for different ngrams in order to work out
+// whether we should use "national broadband network", rather than "national
+// broadband" and "broadband network". In this example with a cutoff of .2,
+// if the longer phrase (ngram of 3) was used 20 times, and "broadband network"
+// was used 22 times (within the cutoff of 20 * 0.2), then it would be removed
+// from the results. If "national broadband" was used more than the cutoff,
+// e.g. 30 times, it would be left in the results.
+exports.combine = function (phrases, cutoff) {
+	var combined = _.clone (phrases);
+
+	_.each (_.keys (phrases), function (phrase) {
+		
+		// Looking for overlap with "smaller" ngrams
+		var ngramToTry = phrase.split (' ').length - 1;
+
+		// Can't have a 0-gram
+		if (ngramToTry < 1) return;
+
+		_.each (natural.NGrams.ngrams (phrase, ngramToTry), 
+			function (ngram) {
+			var subPhrase = ngram.join (' ');
+			
+			if (!phrases[subPhrase]) return;
+
+			var ratio = phrases[phrase] / phrases[subPhrase];
+			if (ratio  >= (1 - cutoff))
+				delete combined[subPhrase];
+		});
+	});
+
+	return combined;
+};
+
+// =============================================================================
+// Extended term-scoring class
+// =============================================================================
+var Tf = function () {
+	natural.TfIdf.call (this);
+};
+
+util.inherits (Tf, natural.TfIdf);
+
+Tf.prototype.listMostFrequentTerms = function (d) {
+	var terms = [];
+	for (var term in this.documents[d]) {
+		terms.push ({
+			term: term, 
+			tf:natural.TfIdf.tf (term, this.documents[d])
+		});
+	}
+
+	return terms.sort (function (x, y) {return y.td - x.tf; });
+};
+
+// =============================================================================
+// Helper functions
+// =============================================================================
+function blacklisted (term) {
+	if (term.match(/^\d+$/) || term.match (/^_/)) {
+		return true;
+	}
+
+	return _.indexOf(stopWords, term) !== -1;
+}
+
+function usePhrase (phrase, options) {	
+	return !_.detect (phrase.split (' '), function (term) {
+		return blacklisted (term);
+	});
+}
+
---
+++ server/search.js
@@ -0,0 +1,202 @@
+var request	= require ('request');
+var string	= require ('string');
+var keyExtract	= require (__dirname + '/../server/keywordExtractor');
+var db		= require (__dirname + '/../server/database');
+var async	= require ('async');
+
+exports.execute = function (req, res) {
+
+	var keywords = [];
+
+	async.series ([
+		function (callback) {
+			console.log ('Getting keywords...');
+			var getKeywords = 'http://127.0.0.1:3000/api/keywords';
+			request.get (getKeywords, function (err, res, body) {
+				keywords = JSON.parse (body);
+				return callback (null);
+			});		
+		},
+		function (callback) {
+
+			async.times (keywords.length, function (n, next) {
+				handleArticles (keywords[n].name, function (err) {
+					next (err);
+				});
+			}, function (err) {
+				return callback (err);
+			});	
+		}
+	],
+		function (err) {
+			if (err) {
+				res.statusCode = 500;
+				return res.send ('Error 500: ' + err);
+			}
+			
+			console.log ('Done!');
+			return true;
+		});
+
+};
+
+function handleArticles (keyword, callback) {
+	console.log ('Getting articles for "' + keyword + '"...');
+	var key =	'YgDPn7G04OM8CF3gbkBtfj4xLCAmBWvWy8xhdu1l108';	
+	var top = 5;
+
+	search =	'https://user:' + key + '@api.datamarket.azure.com' + 
+			'/Bing/Search/v1/Composite?Sources=%27news%27&Query=' + 
+			'%27' + keyword  + '%27&Market=%27es-US%27&' +
+			'$format=JSON';
+
+	request (search, function (err, res, body) {
+		if (err)
+			return callback (err);
+		
+		body = JSON.parse(body);	
+
+		for (var i = 0; i < top; i++) {
+			console.log ('Parsing articles...');
+			var url = body.d.results[0].News[i].Url;
+			parseArticle (url, keyword, saveArticle);
+		}
+
+		return callback (null);
+	});
+}
+
+function parseArticle (url, keyword, callback) {
+
+	var readbilityToken =	'cc589a05ab905a7d8d78ef900a3bb8cf59b8442d'; 
+	var readbilityRequest = 'https://www.readability.com/api/content/v1/' +
+				'parser?url=' + url +
+				'&token=' + readbilityToken;
+ 
+	request (readbilityRequest, function (err, res, body) { 
+
+		body = JSON.parse (body);
+
+		body.title = string (body.title).stripTags ();
+		body.title = body.title.decodeHTMLEntities ();
+
+		body.content = string (body.content).stripTags ();
+		body.content = string (body.content.s).decodeHTMLEntities ();
+
+		var wordCount = body.word_count;
+		var title = body.title.s;
+		var site = body.domain;
+		var url = body.url;
+		var date = body.date_published;
+		
+		if (!date)
+			date = (new Date ()).toUTCString ();
+
+		var bodyKeywords = keyExtract.extract(body.content.s);	
+
+		var article = {
+			"title":		title,	
+			"bodyKeywords":		bodyKeywords,
+			"site":			site,
+			"url":			url,
+			"wordCount":		wordCount,
+			"date":			date,
+			"keyword":		keyword
+		};
+	
+		callback (article);	
+	});
+
+}
+
+function saveArticle (article) {
+	console.log ('Saving article...');
+	article.title = db.escape (article.title);
+
+	var artQuery = 'INSERT INTO articles (title, date, url) VALUES ' +
+			'("' + article.title + '", "' + article.date + '", "' +
+			article.url + '")';
+
+	var keywordReq = 'http://127.0.0.1:3000/api/keywords/' + article.keyword;
+	var keyID = 0;
+	var artID = 0;
+
+	async.series ([
+		function (callback) {
+			request.put (keywordReq, function (err, res, body) {
+				
+				body = JSON.parse (body);
+				if (res.statusCode != 201) {
+					return callback (body, null);
+				}
+
+				return callback (null);
+			});
+		},
+		function (callback) {
+			request.get (keywordReq, function (err, res, body) {
+
+				body = JSON.parse (body);
+				if (res.statusCode != 200) {
+					return callback (body, null);
+				}
+
+				keyID = body.id;
+				
+				return callback (null);
+			});
+		},
+		function (callback) {
+			db.queryDB (artQuery, function (err, rows) {
+
+				if (err) {
+					return callback (err);
+				}
+
+				artID = rows.insertId;
+
+				return callback (null);
+			});
+		},
+		function (callback) {
+			var artKeyQuery =	'INSERT INTO keyArt ' + 
+						'(keyID, artID) VALUES ' +
+						'("' + keyID + '", "' +
+						artID + '")';
+			db.queryDB (artKeyQuery, function (err, rows) {
+
+				if (err) {
+					return callback (err);
+				}
+
+				return callback (null);
+			});
+		},
+		function (callback) {
+			var words = article.bodyKeywords;
+			for (var i = 0; i < words.length; i++) {
+				var term = db.escape (words[i].term);
+				var wordQuery =	'INSERT INTO words (word, ' +
+						'artID, count) VALUES ("' +
+						term + '", "' + artID + 
+						'", "' + words[i].tf + '")';
+
+				db.queryDB (wordQuery, function (err, rows) {
+
+					if (err) {
+						return callback (err);
+					}
+
+					return callback (null);
+				});
+			}
+		}
+	], function (err) {
+		if (err) {
+			console.log (err);	
+		}
+
+		return;
+	});
+
+}
